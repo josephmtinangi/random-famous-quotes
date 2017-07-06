bb162edfaa00a26b55fbeef039224230a8b5290b
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-07-06 15:37:30 +0300

    Show quotes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
-// Create Instance
+axios.defaults.baseURL = 'https://andruxnet-random-famous-quotes.p.mashape.com';
+axios.defaults.headers.common['X-Mashape-Key'] = 'kAica0sHWemshYVboYcEWw510FR8p1Hc5Pfjsn6BS9ArNYkybu';
 
-var instance = axios.create({
-	baseURL: 'https://andruxnet-random-famous-quotes.p.mashape.com',
-	timeout: 1000,
-	headers: {
-		'X-Mashape-Key': 'kAica0sHWemshYVboYcEWw510FR8p1Hc5Pfjsn6BS9ArNYkybu'
+$(document).ready(function(){
+	var author = document.querySelector('#author');
+	var quote = document.querySelector('#quote');
+	var button = document.querySelector('#next');
+	button.addEventListener('click', getQuote);
+
+	function getQuote() {
+		axios.get('/', {
+			params: {
+				cat: 'famous',
+				count: 1
+			}
+		})
+		.then(function (response) {
+			author.innerHTML = response.data.author;
+			quote.innerHTML = response.data.quote;
+		})
+		.catch(function (error) {
+			console.log(error)
+		});		
 	}
 });
-
