c4c65ea4ed1adbb002698abaf57f2964c8693a2c
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-07-06 15:11:58 +0300

    Create instance

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Create Instance
+
+var instance = axios.create({
+	baseURL: 'https://andruxnet-random-famous-quotes.p.mashape.com',
+	timeout: 1000,
+	headers: {
+		'X-Mashape-Key': 'kAica0sHWemshYVboYcEWw510FR8p1Hc5Pfjsn6BS9ArNYkybu'
+	}
+});
+
